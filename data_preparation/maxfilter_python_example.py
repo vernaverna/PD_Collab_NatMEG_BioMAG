7a79616b6fe73fdb0c94fb218fb5d0e26b14d566
vernaverna <88402905+vernaverna@users.noreply.github.com>
2024-06-19 10:17:06 +0300

    Create maxfilter_python_example.py
    add example of maxfiltering handled by python (again for a single subject)

---
+++ data_preparation/maxfilter_python_example.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Example of maxfiltering with python
+Created on Tue Jun 18 10:36:14 2024
+
+@author: heikkiv
+"""
+
+import os
+import subprocess 
+
+# path to bidsified data
+root="/net/theta/fishpool/projects/dbs_pd/neuro-data/dbs_pd/BIDS/"
+
+sub="35P"
+ses="01"
+run="02"
+task="sp"
+trans="default" #, "ses1", "avg" or "init"
+
+
+input_file = f"{root}sub-{sub}/ses-{ses}/meg/sub-{sub}_ses-{ses}_task-{task}_run-{run}_proc-raw_meg.fif"
+output_file = f"{root}derivatives/sub-{sub}/ses-{ses}/meg/sub-{sub}_ses-{ses}_task-{task}_run-{run}_proc-tsss+mc_trans-{trans}_meg.fif"
+log_file = f"{root}derivatives/sub-{sub}/ses-{ses}/meg/sub-{sub}_ses-{ses}_task-{task}_run-{run}_proc-tsss+mc_trans-{trans}_meg_log.log"
+
+#psecif the transformation file / option here 
+trans_file= "default" #"/home/heikkiv/parkinsonsdisease/python/verna/KI_patient_average_rest_ec-trans.fif"
+
+
+# Fit SSS sphere origin to digitization points using fit_sphere_to_isotrak function
+origin_args = ['/neuro/bin/util/fit_sphere_to_isotrak', '-f', input_file]
+output = subprocess.check_output(args=origin_args, text=True).strip()
+origin = output.split(' ')
+
+if trans!="init": #by default, maxfilter transforms the data into intial head position
+    args = ['/neuro/bin/util/mfilter', '-f', input_file, '-o', output_file, '-trans', trans_file, \
+            '-st', '-corr', '0.80', '-ds', '2', '-lpfilt', '100', \
+            '-hpiwin', '1000','-hpistep', '100', '-movecomp', 'inter', \
+            '-v','-autobad','on','-origin'] + origin + ['-in', '8', \
+            '-out', '3', '-frame', 'head', '-force'] #coordinate frame set to head
+                                                        
+else:                                                    
+    args2 = ['/neuro/bin/util/mfilter', '-f', input_file, '-o', output_file, '-hp', \
+            '-st', '-corr', '0.80', '-ds', '2', '-lpfilt', '100', \
+            '-hpiwin', '1000','-hpistep', '100', '-movecomp', 'inter', \
+            '-v','-autobad','on','-origin'] + origin + ['-in', '8', \
+            '-out', '3', '-frame', 'head', '-force']  
+                                                           
+                                                        
+                                                   
+# save the log and errors
+log_output = open(log_file, "w") 
+        
+# run maxfilter
+subprocess.run(args=args, stdout=log_output,stderr=log_output)
